24d2c1aae2066785d76893794a53851540f8b2a2
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2024-09-24 09:12:36 -0500

    Add files via upload

---
+++ smartertrackGit.ps1
@@ -0,0 +1,33 @@
+$APIHost = 'smartertrack.becn.local';
+$HTTPS = $true;
+$API_ST = @{
+    #API Call Default Values
+    authUserName = 'username'
+    authPassword = 'PW'
+    method  = 'post'
+    ContentType    = 'application/json'
+    URI          = 'http' + $(if ($HTTPS -eq $true ) {'s'}) + '://' + $APIHost + '/'
+};
+
+### SHORT ###
+$U = $API_ST.authUserName;
+$P = $API_ST.authPassword;
+
+function UpdateAndCall ( $a ) {
+    $API_ST.URI = 'http' + $(if ($HTTPS -eq $true ) {'s'}) + '://' + $APIHost + $a
+    New-WebServiceProxy -Uri $API_ST.URI -Verbose;
+};
+
+$svcTickets=(UpdateAndCall '/Services/svcTickets.asmx');
+
+$searchResultsNum = $svcTickets.GetTicketPropertiesList($U, $P, 'ticket#')
+
+$results = $searchResultsNum.RequestResults
+
+$frontSubjectLine = 'subject='
+$endSep = 'ticketid='
+
+$seperate = ($results -split $frontSubjectLine)[1]
+
+$Subject = ($seperate -split $endSep)[0]
+$Subject
